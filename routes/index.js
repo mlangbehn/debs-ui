8e89e88894b00e4ca129c93747e67531aceb8636
Michael Flores <floresam@us.ibm.com>
2018-09-29 19:20:02 -0400

    Pulling data from cloudant

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var Cloudant = require('@cloudant/cloudant');
+var cloudant_username = process.env.CLOUDANT_USERNAME;
+var cloudant_password = process.env.CLOUDANT_PASSWORD;
+
+var cloudant = Cloudant({account:cloudant_username, password:cloudant_password});
+
+cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+
+var db = cloudant.db.use('debs');
+var fc = {};
+
+db.find({selector:{type:'Feature'}}, function(er, result) {
+  if(er) {
+    throw er;
+  }
+  fc = {
+    type: "FeatureCollection",
+    "features": result.docs
+  };
+  console.log(JSON.stringify(fc));
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/data', function(req, res, next) {
+  res.send(fc);
 });
 
 module.exports = router;
